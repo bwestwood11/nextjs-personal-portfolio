655591c8e661fd1d6cd7ddbc872eff6e2cd0c05a
unknown <dabrettwestwood@gmail.com>
2022-11-04 01:10:09 -0400

    deploy

---
+++ components/About.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+            <div className='col-span-2'>
+                <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
+                <h2 className='py-4'>Who I am</h2>
+                <p className='py-2 text-gray-600'>// I am not your normal developer</p>
+                <p className='py-2 text-gray-600'>I have spent the last year honing in on my front-end web development skills; however, before that 
+                I was running a small local marketing agency with my brother. I have always had a knack for technology and working with computers.
+                I started with the basics of HTML & CSS. Now I am proficient in Javascript, Typescript, MySQL, React, NextJS & Rest APIs.
+                </p>
+                <p className='py-2 text-gray-600'>Fascinated with how intricate programming can be I was quickly drawn to learn more. I have started
+                freelancing for gigs on Upwork, one of my marketing clients & Fiverr. I have fallen in love with
+                everything about coding.</p>
+                <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects</p>
+            </div>
+            <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+             <Image className='rounded-xl' src='/../public/assets/headshot.png' width='1175' height='1175' alt='/' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ components/Projects.jsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import projectOne from '../public/assets/projects/fitness.png'
+import ProjectItem from './ProjectItem'
+import fitnessImg from '../public/assets/projects/fitness.png'
+import restaurantImg from '../public/assets/projects/restaurantImg.png'
+import cardgameImg from '../public/assets/projects/cardgame.png'
+
+const Projects = () => {
+  return (
+    <div id='projects' className='w-full'>
+         <div className='max-w-[1240px] mx-auto px-2 py-16'>
+          <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
+          <h2 className='py-4'>What I've Built</h2>
+          <div className='grid md:grid-cols-2 gap-8'>
+
+      <ProjectItem 
+      title='Fitness App' 
+      backgroundImg={fitnessImg}
+      projectUrl='/fitness'
+      />
+      <ProjectItem 
+      title='Restaurant Website' 
+      backgroundImg={restaurantImg}
+      projectUrl='/restaurant'
+      />
+      <ProjectItem 
+      title='Magic Card Game' 
+      backgroundImg={cardgameImg}
+      projectUrl='/cardGame'
+      />
+
+    
+
+          </div>
+         </div>
+    </div>
+  )
+}
+
+export default Projects
