7cfc767ef8672a324b15e482b781430d1c529b62
bwestwood11 <dabrettwestwood@gmail.com>
2022-11-08 02:39:33 -0500

    game added

--- pages/cardGame.jsx
+++ pages/cardGame.jsx
@@ -27,7 +27,9 @@ const cardGame = () => {
           <p>Project</p>
           <h2>Overview</h2>
           <p>This project </p>
+          <a href="https://bit.ly/3EeMN0E">
           <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          </a>
           <button className="px-8 py-2 mt-4" py-2 mt-4>
             Code
           </button>
